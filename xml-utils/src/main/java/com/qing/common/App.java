637ee37b144fd9a95137edb0058c262f1d173562
guoqf <guoqf@asiainfo-sec.com>
2019-12-10 10:10:20 +0800

    增加二进制转SQL测试方法

--- xml-utils/src/main/java/com/qing/common/App.java
+++ xml-utils/src/main/java/com/qing/common/App.java
@@ -1,5 +1,14 @@
 package com.qing.common;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.stream.Collectors;
+
 /**
  * Hello world!
  *
@@ -8,6 +17,167 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+
+
+//        System.out.println( "Hello World!" );
+//        String reg = "DW_MEMBER_WAP_.*";
+//        System.out.println("DW_MEMBER_WAP_)".matches(reg));
+
+
+
+
+//        int[] arr = {1, 2, 2, 3};
+//        HashSet<Integer> hashSet = new HashSet<Integer>();
+//        for (int ar:arr
+//             ) {
+//            hashSet.add(ar);
+//        }
+//        System.out.println(hashSet);
+/*
+        int[] arr = {1, 2, 3, 3, 3, 5, 9};
+        int target = 3;
+        App app = new App();
+        System.out.println(app.binarySearchMinIndex(arr, target));*/
+
+//        testLongArray();
+//        convertSingleDataToSql();
+//        readDataAndWriteSql();
+//        readMultiFilesAndWriteSql();
+
+        List<Long> longs = Arrays.asList(1L, 2L, 3L, 4L);
+        System.out.println("longs: " + longs);
+        List<Long> collect = longs.stream().filter(item -> item > 2L).collect(Collectors.toList());
+        System.out.println("Collect: "+collect);
+        System.out.println("longs: " + longs);
+
+    }
+
+    private static void convertSingleDataToSql() {
+//        String dataStr = "00 83 00 00 06 00 00 00 00 00 03 3E 1D 03 00 00 00 01 00 00 00 01 60 00 00 00 01 01 20 00 00 00 01 01 40 0B 00 00 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 02 00 00 00 00 00 00 00 20 73 65 6C 65 63 74 20 2A 20 66 72 6F 6D 20 75 73 65 72 69 6E 66 6F 2E 63 75 73 74 6F 6D 65 72 0A ";
+        String dataStr = "00 98 00 00 06 00 00 00 00 00 03 52 02 01 1B 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 01 1B 00 00 00 01 2D 00 00 00 01 1B 00 00 00 30 11 00 00 01 21 00 00 00 01 24 00 00 00 00 00 00 00 00 00 00 00 00 00 01 23 01 00 00 01 09 48 57 5F 5A 48 41 4F 58 4A 09 5A 57 58 36 30 35 32 38 39 0F 43 48 49 4E 41 5C 5A 57 58 36 30 35 32 38 39 09 7A 57 58 36 30 35 32 38 39 0B 31 34 38 32 30 3A 31 33 34 34 34 0C 70 6C 73 71 6C 64 65 76 2E 65 78 65 ";
+//        String dataStr = "73 65 6C 65 63 74 20 2A 20 66 72 6F 6D 20 75 61 70 5F 6D 61 69 6E 5F 61 63 63 74 5F 72 6F 6C 65 0A";
+//        String dataStr = "00 61 00 00 06 00 00 00 00 00 11 69 18 01 01 01 01 01 03 5E 19 02 80 61 00 01 01 21 01 01 0D 01 01 00 00 00 00 00 00 01 00 01 01 01 00 00 01 01 21 73 65 6C 65 63 74 20 2A 20 66 72 6F 6D 20 75 61 70 5F 6D 61 69 6E 5F 61 63 63 74 5F 72 6F 6C 65 0A 01 01 00 00 00 00 00 00 01 01 00 00 00 00 00";
+        byte[] bytes = ByteUtil.hexStr2Bytes(dataStr);
+        System.out.println(new String(bytes, StandardCharsets.UTF_8));
+    }
+
+
+    private static void convertMultiDataToSql() {
+        String filePath = "C:\\Users\\tsing\\Desktop\\033edata\\multi_data.log";
+        File file = new File(filePath);
+        try {
+            List<String> lines = FileUtils.readLines(file, StandardCharsets.UTF_8);
+//            byte[] bytes = ByteUtil.hexStr2Bytes(dataStr);
+//            System.out.println(new String(bytes, StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private static void readMultiFilesAndWriteSql() {
+        String dir = "C:\\Users\\tsing\\Desktop\\033edata\\";
+        String[] fileNames = {"1.txt", "2.txt", "3.txt", "4.txt"};
+        Iterator<File> txtIt = FileUtils.iterateFiles(new File(dir), null, false);
+        while (txtIt.hasNext()) {
+            File next = txtIt.next();
+            int lineNum=1;
+            try {
+                List<String> lines = FileUtils.readLines(next, StandardCharsets.UTF_8);
+                for (String line : lines
+                ) {
+                    handleOneLine(lineNum++, line);
+                }
+                System.out.println();
+                System.out.println(next.getName()+" end : ===============================");
+                System.out.println();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+    private static void readDataAndWriteSql() {
+
+//        String filePath = "C:\\Users\\tsing\\Desktop\\oracle_client_data1.txt";
+        String filePath = "F:\\Downloads\\TencentFiles\\344041962\\FileRecv\\7.txt";
+//        String filePath = "F:\\Downloads\\TencentFiles\\344041962\\FileRecv\\8.txt";
+        int lineNum=1;
+        try {
+            List<String> lines = FileUtils.readLines(new File(filePath), StandardCharsets.UTF_8);
+            for (String line : lines
+                 ) {
+                 handleOneLine(lineNum++, line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void handleOneLine(int lineNum, String line) {
+        String dataStr;
+        String tag = "==>";
+        int tagLength = tag.length();
+        int i = line.indexOf(tag);
+        if (i < 0) {
+            return ;
+        }
+        dataStr = line.substring(i + tagLength);
+
+        byte[] bytes = ByteUtil.hexStr2Bytes(dataStr);
+        System.out.println(lineNum + ":--->");
+        System.out.println(new String(bytes, StandardCharsets.UTF_8));
+
+    }
+
+    private static void testLongArray() {
+        Long[] ids = {1L, 2L, 3L,2L,3L};
+
+        Long[] longs = Arrays.stream(ids).distinct().toArray(Long[]::new);
+
+        for (Long id :
+                longs) {
+            System.out.println(id);
+        }
+    }
+
+    public int binarySearchMinIndex(int[] arr, int target){
+        if(ArrayUtils.isEmpty(arr)){
+            return -1;
+        }
+
+        if(arr.length==1){
+            return arr[0] == target ? 0 : -1;
+        }
+
+        return binarySearch(arr,target,0, arr.length);
+    }
+
+    private int binarySearch(int[] arr, int target, int from, int to){
+        if(arr.length==1){
+            return arr[0] == target ? 0 : -1;
+        }
+        int mid = from + (to - from)/2;
+
+        if(arr[mid] == target){
+            if(from < mid && arr[mid -1]==target){
+                return binarySearch(arr, target, from, mid);
+            }
+            return mid;
+        }else if(arr[mid] > target){
+            if(mid>from){
+                return binarySearch(arr,target,from,mid);
+            }
+            return -1;
+        }else{
+            if(to > mid+1){
+                return binarySearch(arr, target,mid+1,to);
+            }
+            return -1;
+        }
     }
 }
---
+++ xml-utils/src/main/java/com/qing/common/ByteUtil.java
@@ -0,0 +1,183 @@
+package com.qing.common;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+public class ByteUtil {
+    public ByteUtil() {
+    }
+
+    public static byte[] short2Byte(short a) {
+        byte[] b = new byte[]{(byte)(a >> 8), (byte)a};
+        return b;
+    }
+
+    public static void short2Byte(short a, byte[] b, int offset) {
+        b[offset] = (byte)(a >> 8);
+        b[offset + 1] = (byte)a;
+    }
+
+    public static short byte2Short(byte[] b) {
+        return (short)((b[0] & 255) << 8 | b[1] & 255);
+    }
+
+    public static int byteToInt(byte[] b) {
+        return b[0] & 255 | (b[1] & 255) << 8 | (b[2] & 255) << 16;
+    }
+
+    public static int byte2int_le(byte[] bt, int pos) {
+        int size = bt[pos] & 255;
+        size |= (bt[pos + 1] & 255) << 8;
+        size |= (bt[pos + 2] & 255) << 16;
+        size |= (bt[pos + 3] & 255) << 24;
+        return size;
+    }
+
+    public static short byte2Short(byte[] b, int offset) {
+        return (short)((b[offset] & 255) << 8 | b[offset + 1] & 255);
+    }
+
+    public static void long2Byte(long a, byte[] b, int offset) {
+        b[offset + 0] = (byte)((int)(a >> 56));
+        b[offset + 1] = (byte)((int)(a >> 48));
+        b[offset + 2] = (byte)((int)(a >> 40));
+        b[offset + 3] = (byte)((int)(a >> 32));
+        b[offset + 4] = (byte)((int)(a >> 24));
+        b[offset + 5] = (byte)((int)(a >> 16));
+        b[offset + 6] = (byte)((int)(a >> 8));
+        b[offset + 7] = (byte)((int)a);
+    }
+
+    public static long byte2Long(byte[] b, int offset) {
+        return ((long)b[offset + 0] & 255L) << 56 | ((long)b[offset + 1] & 255L) << 48 | ((long)b[offset + 2] & 255L) << 40 | ((long)b[offset + 3] & 255L) << 32 | ((long)b[offset + 4] & 255L) << 24 | ((long)b[offset + 5] & 255L) << 16 | ((long)b[offset + 6] & 255L) << 8 | ((long)b[offset + 7] & 255L) << 0;
+    }
+
+    public static long byte2Long(byte[] b) {
+        return (long)((b[0] & 255) << 56 | (b[1] & 255) << 48 | (b[2] & 255) << 40 | (b[3] & 255) << 32 | (b[4] & 255) << 24 | (b[5] & 255) << 16 | (b[6] & 255) << 8 | b[7] & 255);
+    }
+
+    public static byte[] long2Byte(long a) {
+        byte[] b = new byte[]{(byte)((int)(a >> 56)), (byte)((int)(a >> 48)), (byte)((int)(a >> 40)), (byte)((int)(a >> 32)), (byte)((int)(a >> 24)), (byte)((int)(a >> 16)), (byte)((int)(a >> 8)), (byte)((int)(a >> 0))};
+        return b;
+    }
+
+    public static int byte2Int(byte[] b) {
+        return (b[0] & 255) << 24 | (b[1] & 255) << 16 | (b[2] & 255) << 8 | b[3] & 255;
+    }
+
+    public static int byte2Int(byte[] b, int offset) {
+        return (b[offset++] & 255) << 24 | (b[offset++] & 255) << 16 | (b[offset++] & 255) << 8 | b[offset++] & 255;
+    }
+
+    public static byte[] int2Byte(int a) {
+        byte[] b = new byte[]{(byte)(a >> 24), (byte)(a >> 16), (byte)(a >> 8), (byte)a};
+        return b;
+    }
+
+    public static void int2Byte(int a, byte[] b, int offset) {
+        b[offset++] = (byte)(a >> 24);
+        b[offset++] = (byte)(a >> 16);
+        b[offset++] = (byte)(a >> 8);
+        b[offset++] = (byte)a;
+    }
+
+    public static int onebyte2int(byte[] b, int off) {
+        return 255 & b[off];
+    }
+
+    public static void int2onebyte(int n, byte[] b, int off) throws Exception {
+        if (n >= 0 && n <= 255) {
+            b[off] = 0;
+            b[off] = (byte)(b[off] | n);
+        } else {
+            throw new Exception(n + " exceed unsigned char range [0, 255]!");
+        }
+    }
+
+    public static void int2twobyte(int n, byte[] b, int off, boolean isLowerBefore) throws Exception {
+        if (n >= 0 && n <= 65535) {
+            if (isLowerBefore) {
+                b[off] = (byte)(n >> 8);
+                b[off + 1] = (byte)n;
+            } else {
+                b[off + 1] = (byte)(n >> 8);
+                b[off] = (byte)n;
+            }
+
+        } else {
+            throw new Exception(n + " exceed unsigned char range [0, 65535]!");
+        }
+    }
+
+    public static byte[] hexStr2Bytes(String s) {
+        String[] strings = s.split(" ");
+        byte[] data = new byte[strings.length];
+
+        for(int i = 0; i < strings.length; ++i) {
+            data[i] = (byte)((Character.digit(strings[i].charAt(0), 16) << 4) + Character.digit(strings[i].charAt(1), 16));
+        }
+
+        return data;
+    }
+
+    public static final String toHexString(byte[] b) {
+        String hexChar = "0123456789ABCDEF";
+        StringBuffer sb = new StringBuffer();
+
+        for(int i = 0; i < b.length; ++i) {
+            sb.append("0123456789ABCDEF".charAt(b[i] >> 4 & 15));
+            sb.append("0123456789ABCDEF".charAt(b[i] & 15));
+            sb.append(" ");
+        }
+
+        return sb.toString();
+    }
+
+    public static String toHexString(String str) {
+        StringBuffer sb = new StringBuffer();
+
+        for(int i = 0; i < str.length(); ++i) {
+            int ch = str.charAt(i);
+            sb.append(Integer.toHexString(ch)).append(" ");
+        }
+
+        return sb.toString();
+    }
+
+    public static boolean isEmpty(byte[] bytes) {
+        return bytes == null || bytes.length < 1;
+    }
+
+    public static String byte2String(byte[] bytes, String encode) {
+        if (!isEmpty(bytes) && !StringUtils.isEmpty(encode)) {
+            String returnString = null;
+
+            try {
+                returnString = new String(bytes, encode);
+            } catch (UnsupportedEncodingException var4) {
+                var4.printStackTrace();
+            }
+
+            return returnString;
+        } else {
+            return null;
+        }
+    }
+
+    public static byte[] listByte2bytes(List<Byte> listByte) {
+        byte[] bytes = new byte[listByte.size()];
+
+        for(int i = 0; i < listByte.size(); ++i) {
+            bytes[i] = (Byte)listByte.get(i);
+        }
+
+        return bytes;
+    }
+}
